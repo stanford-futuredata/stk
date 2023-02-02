ba828b7be28203bf9f9cb3cfa92738eeea1155c5
Trevor Gale <tgale@stanford.edu>
2023-02-02 13:27:52 -0800

    Make STK pip installable with source distribution.

--- setup.py
+++ setup.py
@@ -1,21 +1,66 @@
+import os
+from pathlib import Path
 from setuptools import setup, find_packages
+import subprocess
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
-# TODO(tgale): Add the Sputnik build to this script so we don't
-# need to already have it installed.
+
+# NOTE: The 'glob' package didn't get the root_dir argument until 3.10.
+def glob(ext, root_dir):
+    file_list = []
+    for dirname, _, files in os.walk(root_dir):
+        file_list += [
+            f"{dirname}/{f}" for f in files if f.endswith(ext)
+        ]
+    return file_list
+
+# HACK: Upgrade our CUTLASS dependency so we do not need to monkey-patch
+# the source to build.
+subprocess.call([
+    "sed", "-i", "1i#include \"cutlass/layout/matrix.h\"",
+    "third_party/sputnik/third_party/cutlass/include/cutlass/gemm/gemm.h"
+])
+
+def generate_sputnik_sources():
+    sources = glob(".cu", "third_party/sputnik/sputnik/block/")
+    sources += glob(".cc", "third_party/sputnik/sputnik/")
+
+    sputnik_exclude_suffixes = [
+        "_test.cu",
+        "_benchmark.cu",
+        "matrix_utils.cu",
+    ]
+    def filter_sources(f):
+        return not any([
+            f.endswith(suffix) for suffix in sputnik_exclude_suffixes
+        ])
+
+    # Filter test and benchmark files.
+    sources = list(filter(filter_sources, sources))
+    return sources
+
+# NOTE: Initialize all submodules recursively prior to build.
+subprocess.call(["git", "submodule", "update", "--init", "--recursive"])
+
+cwd = Path(os.path.dirname(os.path.abspath(__file__)))
+print(f"{cwd}/third_party/sputnik/")
 ext_modules = [
     CUDAExtension(
         "sputnik_backend",
-        ["csrc/backend.cc"],
-        include_dirs = ["/usr/local/sputnik/include"],
-        libraries = ["sputnik"],
-        library_dirs = ["/usr/local/sputnik/lib"],
-        extra_compile_args={"cxx": ["-fopenmp", "-fPIC"]})
+        ["csrc/backend.cc"] + generate_sputnik_sources(),
+        include_dirs = [
+            f"{cwd}/third_party/sputnik/",
+            f"{cwd}/third_party/sputnik/third_party/cutlass/include/"
+        ],
+        extra_compile_args={
+            "cxx": ["-fopenmp", "-fPIC", "-Wno-strict-aliasing"]
+        }
+    )
 ]
 
 setup(
     name="stk",
-    version="0.0.1",
+    version="0.0.3",
     author="Trevor Gale",
     description="A PyTorch library for block-sparse matrices and block-sparse matrix multiplication.",
     long_description=open('README.md').read(),
@@ -23,5 +68,5 @@ setup(
     packages=find_packages(),
     ext_modules=ext_modules,
     cmdclass={"build_ext": BuildExtension},
-    install_requires=[l.strip() for l in open('requirements.txt', 'r').readlines()],
+    install_requires=["absl-py", "numpy", "torch"],
 )
