2669b6aabdcb032464dcbc39eb0b69e73dfe17a8
Priya Mishra <26priya11@gmail.com>
2023-04-18 15:43:51 -0700

    DDS kernel implementation

--- stk/backend/__init__.py
+++ stk/backend/__init__.py
@@ -1,4 +1,4 @@
 from stk.backend.sputnik import dsd
-from stk.backend.triton_kernels import sdd, dsd
+from stk.backend.triton_kernels import sdd, dsd, dds, matmul
 from stk.ops.matrix_ops import row_indices, to_dense, to_sparse, ones_like, sum
 from stk.ops.linear_ops import dsd, dds, sdd, ssd, sds, dss
